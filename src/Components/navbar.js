7ca84f47c6bc45b64194d37686970fd7238d6376
the_ellusionist <adarshv198@gmail.com>
2020-05-08 22:47:08 +0530

    Changed

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import M from  'materialize-css/dist/js/materialize.min.js';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import 'materialize-css/dist/css/materialize.min.css';
+
 class Navbar extends React.Component{
      componentDidMount() {
         var elem = document.querySelector(".sidenav");
@@ -21,17 +22,17 @@ class Navbar extends React.Component{
                         <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i
                             className="material-icons">menu</i></a>
                         <ul className="right hide-on-med-and-down">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/problem">Problems Solved</Link></li>
-                            <li><Link to='/compare_problems'>Compare Problems</Link></li>
+                            <li><Link target="_blank" to="/">Home</Link></li>
+                            <li><Link target="_blank" to="/problem">Problems Solved</Link></li>
+                            <li><Link target="_blank" to='/compare_problems'>Compare Problems</Link></li>
                         </ul>
                     </div>
                 </nav>
 
                 <ul className="sidenav" id="mobile-demo">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/problem">Problems Solved</Link></li>
-                    <li><Link to='/compare_problems'>Compare Problems</Link></li>
+                    <li><Link target="_blank" to="/">Home</Link></li>
+                    <li><Link target="_blank" to="/problem">Problems Solved</Link></li>
+                    <li><Link target="_blank" to='/compare_problems'>Compare Problems</Link></li>
                 </ul>
 
             </div>
