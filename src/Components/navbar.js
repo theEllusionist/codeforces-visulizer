ad623fb67c18d046c4c24993b78f8d2498fa5c0c
the_ellusionist <adarshv198@gmail.com>
2020-05-08 22:57:23 +0530

    Changed

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -22,17 +22,17 @@ class Navbar extends React.Component{
                         <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i
                             className="material-icons">menu</i></a>
                         <ul className="right hide-on-med-and-down">
-                            <li><Link target="_blank" to="/">Home</Link></li>
-                            <li><Link  to="/problem" target="_blank" onClick={(event) => {event.preventDefault(); window.open(this.makeHref("/problem"));}}>Problems Solved</Link></li>
-                            <li><Link target="_blank" to='/compare_problems'>Compare Problems</Link></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/problem">Problems Solved</Link></li>
+                            <li><Link to='/compare_problems'>Compare Problems</Link></li>
                         </ul>
                     </div>
                 </nav>
 
                 <ul className="sidenav" id="mobile-demo">
-                    <li><Link target="_blank" to="/">Home</Link></li>
-                    <li><Link target="_blank" to="/problem">Problems Solved</Link></li>
-                    <li><Link target="_blank" to='/compare_problems'>Compare Problems</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/problem">Problems Solved</Link></li>
+                    <li><Link to='/compare_problems'>Compare Problems</Link></li>
                 </ul>
 
             </div>
